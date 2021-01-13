d0f2a6868e0292d8a1e1dc21f14be2d2861cc839
chenshuo-as <44311021+chenshuo-as@users.noreply.github.com>
2021-01-13 13:49:25 +0800

    Create changboot.sh

---
+++ changboot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+new_chang=Advanced options for Debian GNU/Linux>Debian GNU/Linux, with Linux 4.14.182-bbrplus
+tmp_new="GRUB_DEFAULT := \"$new_chang\""
+sed -i "/^GRUB_DEFAULT/c$tmp_new" /etc/default/grub
