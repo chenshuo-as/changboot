09bc2f5df030863b61434ae878f0d2965eaa2c11
chenshuo-as <44311021+chenshuo-as@users.noreply.github.com>
2021-01-13 14:02:11 +0800

    Update changboot.sh

--- changboot.sh
+++ changboot.sh
@@ -4,3 +4,10 @@
 new_chang="Advanced options for Debian GNU/Linux>Debian GNU/Linux, with Linux 4.14.182-bbrplus"
 tmp_new="GRUB_DEFAULT := \"$new_chang\""
 sed -i "/^GRUB_DEFAULT/c$tmp_new" /etc/default/grub
+update-grub
+sleep 1
+read -p "需要重启VPS，是否现在重启 ? [Y/n] :" yn
+[ -z "${yn}" ] && yn="y"
+ if [[ $yn == [Yy] ]]; then
+ echo -e "${Info} VPS 重启中..."
+ reboot
