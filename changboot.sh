2463f21dfd043f1f953eadc30ec93f0f480183a3
chenshuo-as <44311021+chenshuo-as@users.noreply.github.com>
2021-01-13 14:28:15 +0800

    Update changboot.sh

--- changboot.sh
+++ changboot.sh
@@ -2,7 +2,7 @@
 
 
 new_chang="Advanced options for Debian GNU/Linux>Debian GNU/Linux, with Linux 4.14.129-bbrplus"
-tmp_new="GRUB_DEFAULT= \"$new_chang\""
+tmp_new="GRUB_DEFAULT=\"$new_chang\""
 sed -i "/^GRUB_DEFAULT/c$tmp_new" /etc/default/grub
 update-grub
 sleep 1
